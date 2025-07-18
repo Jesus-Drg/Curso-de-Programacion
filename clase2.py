ee74c06ce2d8e2b5f9ef39fa4eef0e8c9ae46fad
Jesus-Drg <jr8736150@gmail.com>
2025-07-18 13:54:44 -0400

    h

---
+++ clase2.py
@@ -0,0 +1,55 @@
+#numero1 = 10
+#numero2 = 20
+
+#if numero1 < numero2:
+    #print(f"{numero1} es menor que {numero2}.")
+#else:
+    #print(f"{numero1} es mayor que {numero2}")
+
+
+#numero3 = 10
+#numero4 = 15
+#numero5 = 20
+
+#if (numero5 > numero4 and numero5 > numero3) or (numero3 < numero4):
+    #print(f"{numero5} es mayor que {numero4} y mayor que {numero3}, o {numero3} es menor que {numero4}.")
+#else:
+    #print("Ninguna de las operaciones se cumpliò")
+
+#nombre = input("Escriba su nombre: ")
+#apellido = input("Escriba su apellido: ")
+#edad = input("Escriba su edad: ")
+#pais = input("Escriba su pais de origen: ")
+
+#print(f"Su nombre es {nombre}, su apellido {apellido}, su edad es de {edad} años y su pais de origen es {pais}")
+
+#numero6 = int(input("Escribe el valor de numero6: "))
+#numero7 = int(input("Escribe el valor de numero7: "))
+
+#suma = numero6+numero7
+
+#resultado = suma
+
+#print("El resultado es: ", resultado)
+
+calificacion = int(input("Escribe el valor de calificacion: "))
+
+if calificacion > 90:
+    print("A")
+elif calificacion >= 80:
+    print("B")
+elif calificacion >= 70:
+    print("C")
+elif calificacion >=60:
+    print("D")
+else:
+    print("Reprobado")
+
+numero = int(input("Escribe el valor del numero: "))
+
+if numero>= 0:
+    print("Es positivo")
+else:
+    print("Es negativo")
+
+
